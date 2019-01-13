ec552d146a1cd544f0fbb99e5ab09d38f68efedb
Nathaniel May <nathanieldmay@gmail.com>
2019-01-12 21:00:58 -0500

    refactored to be more functional. not done yet.

--- src/main/scala/com/nathanielmay/randpix/RandPix.scala
+++ src/main/scala/com/nathanielmay/randpix/RandPix.scala
@@ -1,18 +1,19 @@
 package com.nathanielmay.randpix
 
+// Java
 import java.io.File
 import java.nio.file.{Path, Paths}
-import java.util.concurrent.Executors
 
-import cats.effect.{ExitCode, IO, IOApp, Resource}
+// Scala
+import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits._
-import fs2.{io, text}
+
+// Project
 import util.Shuffle.shuffle
-import util.BufferedStream, BufferedStream.next
+import util.BufferedStream, BufferedStream.{next, prev}
+import util.CommandLine, CommandLine.{printlnSafe, readLineSafe}
 
-import scala.concurrent.ExecutionContext
-
-object RandPix {
+object RandPix extends IOApp {
 
   def getFiles(f: File): Stream[Path] =
     if (f.isDirectory) f.listFiles()
@@ -21,44 +22,23 @@ object RandPix {
       .map(f => Paths.get(f.getAbsolutePath))
     else throw new Exception("not a valid directory") // TODO
 
-//  def next(pos: Int, buffer: Vector[Path], stream: Stream[Path]): (Int, Vector[Path], Stream[Path], Option[Path]) =
-//    if (pos+1 < buffer.size) (pos+1, buffer, stream, buffer.get(pos+1))
-//    else stream match {
-//      case Stream.Empty => (pos,   buffer,      stream, buffer.get(pos))
-//      case p #:: ps     => (pos+1, buffer :+ p, ps,     Some(p))
-//    }
-//
-//  def prev(pos: Int, buffer: Vector[Path], stream: Stream[Path]): (Int, Vector[Path], Stream[Path], Option[Path]) =
-//    if (pos > 0) (pos-1, buffer, stream, buffer.get(pos-1))
-//    else (pos, buffer, stream, buffer.headOption)
+  override def run(args: List[String]): IO[ExitCode] = {
+    def printLoop[T](buffer: BufferedStream[T]): IO[Unit] = for {
+      _  <- IO(println("[n]ext or [p]revious item?"))
+      np <- CommandLine.readUntil(List("n", "p"), "[n] = next, [p] = previous")
+      _  <- if (np == "n") next.run(buffer)
+      else prev.run(buffer)
+    } yield Unit
 
-  def main(args: Array[String]): Unit = {
-    println("Path with pictures:")
-    val path = scala.io.StdIn.readLine()
-    val file = new File(path)
-    println(s"path contains ${file.listFiles.length} files")
+    def userInterface: IO[Unit] = for {
+      _      <- printlnSafe("Path with pictures:")
+      path   <- readLineSafe
+      file   =  new File(path)
+      _      <- printlnSafe(s"path contains ${file.listFiles.length} files")
+      stream =  shuffle(getFiles(file)).eval(new scala.util.Random(System.nanoTime())) //TODO do I want that here?
+      _      <- printLoop(BufferedStream(stream))
+    } yield Unit
 
-    val shuffled: Stream[Path] = shuffle(getFiles(file)).eval(new scala.util.Random(System.nanoTime()))
-    val buffer   = Vector()
-    val pos      = 0
-
-    def printLoop[T](buffer: BufferedStream[T]): Unit = {
-      println("[n]ext or [p]revious item?")
-      var line = scala.io.StdIn.readLine()
-      while(!List("n", "p").contains(line)){
-        println("n = next, p = previous")
-        line = scala.io.StdIn.readLine()
-      }
-    }
-
-    println(s"first file: ${next.eval(BufferedStream(shuffled))}") // TODO
-
-//    for {
-//      file <- IO {
-//        new File(args(0).toString)
-//      }
-//      files =  getFiles(file)
-//      shuffled <- shuffle(files)
-//    } yield Unit
+    userInterface.map(_ => ExitCode.Success)
   }
 }
--- src/main/scala/util/CommandLine.scala
+++ src/main/scala/util/CommandLine.scala
@@ -5,9 +5,13 @@ import cats.effect.IO
 object CommandLine {
 
   def readUntil(acceptable: List[String], errorMessage: String): IO[String] =
-    IO(scala.io.StdIn.readLine())
-    .flatMap { line => if(!acceptable.contains(line))
-                         IO(println(errorMessage))
-                           .flatMap { _ => readUntil(acceptable, errorMessage) }
-                       else IO(line) }
+    readLineSafe.flatMap {
+      line => if(!acceptable.contains(line))
+                printlnSafe(errorMessage)
+                  .flatMap { _ => readUntil(acceptable, errorMessage) }
+              else IO(line) }
+
+  def printSafe  (a: Any*): IO[Unit] = IO(print(a))
+  def printlnSafe(a: Any*): IO[Unit] = IO(println(a))
+  def readLineSafe = IO(scala.io.StdIn.readLine())
 }
