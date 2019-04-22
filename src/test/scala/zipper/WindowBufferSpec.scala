f0c9017cc1b9a8d2b2217a423ca8714bb86c5e16
Nathaniel May <nathanieldmay@gmail.com>
2019-04-21 21:24:55 -0400

    added test for WindowBuffer toString

---
+++ src/test/scala/zipper/WindowBufferSpec.scala
@@ -0,0 +1,30 @@
+package zipper
+
+// scalatest
+import org.scalatest.Matchers._
+import org.scalatest._
+
+// scala
+import cats.data.Writer
+import cats.effect.IO
+import cats.implicits._
+
+
+class WindowBufferSpec extends FlatSpec {
+
+  "WindowBuffer" should "have the right toString" in {
+    val w0 = WindowBuffer(0, Unlimited)
+    val w1 = w0.next(1)
+    val w2 = w1 match {
+      case ww: NoRight[Int]  => ww.next(2)
+      case ww: HasRight[Int] => ww.next
+    }
+    val w3 = w2 match {
+      case ww: NoLeft[Int]  => ww.prev(1)
+      case ww: HasLeft[Int] => ww.prev
+    }
+
+    w3.toString.startsWith("WindowBuffer: Limit: zipper.") shouldBe true
+    w3.toString.endsWith("[0 -> 1 <- 2]") shouldBe true
+  }
+}
